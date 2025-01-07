e8626ff5a60e73b91380b5a8b0d9bd2945172376
Harshit <itsharshitraj@gmail.com>
2025-01-07 10:56:10 +0530

    Addition functionality added

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Addition{
+public static void main(String args[]){
+   System.out.println("Enter three integer to add");
+   Scanner sc=new Scanner(System.in);
+   int a=sc.nextInt();
+   int b=sc.nextInt();  
+   int c=sc.nextInt();  
+
+System.out.println("Addition : "+ (a+b+c));
+
+}
+}
